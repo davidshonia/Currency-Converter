aa637d2cb0bdd099cd38ecc96cc32af91cd3ceda
Nika <nikakvrivishvili777@gmail.com>
2021-12-05 14:49:57 +0400

    added labels

--- currency_converter.py
+++ currency_converter.py
@@ -2,6 +2,11 @@ from tkinter import *
 
 root = Tk()
 
+label_lari = Label(root, text = "lari")
+label_lari.pack()
+
+label_dollar = Label(root, text = "dollar")
+label_dollar.pack()
+
 
 root.mainloop()
-
