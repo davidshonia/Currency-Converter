db2630072c4217c59ea16b62837c36789bbc5e34
davidshonia <dupecube1@gmail.com>
2021-12-05 16:06:25 +0400

    parsing buying rates

--- currency_converter.py
+++ currency_converter.py
@@ -1,4 +1,24 @@
 from tkinter import *
+from bs4 import BeautifulSoup
+import requests
+
+url ="https://halykbank.ge/en/individuals/currency"
+result = requests.get(url)
+doc = BeautifulSoup(result.text, "html.parser")
+
+soup = doc.find_all("td")
+
+dolar = soup[1].text
+euro = soup[4].text
+pound = soup[8].text
+
+
+k = float(input("Lari: "))
+print(float(dolar)*k)
+print(float(euro)*k)
+print(float(pound)*k)
+##################################################
+
 
 
 
